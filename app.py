38a58b880a69cf5a3f00e5fc573ea73cf8aadf47
shatha-marzuq <shathamarzuq5@gmail.com>
2026-03-15 01:52:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown("""
     --border:  #E5E7EB;  /* حدود رمادي ناعم */
     --muted:   #6B7280;  /* نص ثانوي */
     --teal:    #5b5b5b ;  /* أزرار أزرق كحلي */
-    --teal2:   #5b5b5b ;  /* hover أفتح */
+    --teal2:   #2563EB;  /* hover أفتح */
     --user-bg: #DBEAFE;  /* فقاعة المستخدم أزرق فاتح */
     --ai-bg:   #FFFFFF;  /* فقاعة الـ AI أبيض */
 }
