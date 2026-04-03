cd4db89ab6010bdf5e495a1ce6f674f98d9e059a
shatha-marzuq <shathamarzuq5@gmail.com>
2026-04-03 22:18:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -299,7 +299,7 @@ hr { border-color: var(--line) !important; }
     letter-spacing: 1.2px;
     color: #9a9a92;
     text-transform: uppercase;
-    margin-bottom: 2px; /* قلل الرقم هنا من 8px إلى 2px */
+    margin-bottom: 8px; 
     display: block;
 }
 </style>
